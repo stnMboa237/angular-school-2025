520aec9a1dad57175a2054ac5315fa5a6be059ea
Stephane NGASSA <stephane.ngassa@sword-group.com>
2025-06-11 13:19:57 +0200

    bootstrap de l'appli avec l'array bootstrap declarer dans @NgModule de l'AppModule

--- apps/01-hands-on/src/app/app.module.ts
+++ apps/01-hands-on/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { ApplicationRef, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialDesignModule } from './material-design.module';
@@ -8,9 +8,8 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MaterialDesignModule],
   providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
-  ngDoBootstrap(appRef: ApplicationRef) {
-    appRef.bootstrap(AppComponent);
-  }
+
 }
